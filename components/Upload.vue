305e280b35cf9016ec835aad5a85f37df1696aef
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-11-07 05:47:47 +0200

    Video upload complete

---
+++ components/Upload.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="mt-12">
+    <form
+      action="#"
+      method="POST"
+      class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+      @submit.prevent="submit"
+    >
+      <div class="sm:col-span-2">
+        <label for="file" class="block text-sm font-medium text-gray-700"
+          >File</label
+        >
+        <div class="mt-1">
+          <input
+            accept="video/*"
+            @change="handleFile"
+            type="file"
+            name="file"
+            id="file"
+            class="
+              py-3
+              px-4
+              block
+              w-full
+              shadow-sm
+              focus:ring-indigo-500 focus:border-indigo-500
+              border-gray-300
+              rounded-md
+            "
+          />
+        </div>
+      </div>
+      <div class="sm:col-span-2">
+        <p
+          v-if="uploading"
+          class="text-center text-sm font-semibold text-gray-700"
+        >
+          Uploading...
+        </p>
+        <button
+          v-else
+          type="submit"
+          class="
+            w-full
+            inline-flex
+            items-center
+            justify-center
+            px-6
+            py-3
+            border border-transparent
+            rounded-md
+            shadow-sm
+            text-base
+            font-medium
+            text-white
+            bg-indigo-600
+            hover:bg-indigo-700
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-indigo-500
+          "
+        >
+          Upload
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      uploading: false,
+      uploadVideo: null,
+    };
+  },
+  methods: {
+    async handleFile(e) {
+      this.uploadVideo = e.target.files[0];
+    },
+    async readData(f) {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.readAsDataURL(f);
+      });
+    },
+    async submit() {
+      this.uploading = true;
+      const videoData = await this.readData(this.uploadVideo);
+      this.cloudinaryVideo = await this.$cloudinary.upload(videoData, {
+        upload_preset: "default-preset",
+        folder: "nuxtjs-video-trimmer",
+      });
+      console.log(this.cloudinaryVideo);
+      this.uploading = false;
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,146 @@
 <template>
-  <Tutorial/>
+  <div class="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
+    <div class="relative max-w-xl mx-auto">
+      <svg
+        class="absolute left-full transform translate-x-1/2"
+        width="404"
+        height="404"
+        fill="none"
+        viewBox="0 0 404 404"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="85737c0e-0916-41d7-917f-596dc7edfa27"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect
+              x="0"
+              y="0"
+              width="4"
+              height="4"
+              class="text-gray-200"
+              fill="currentColor"
+            />
+          </pattern>
+        </defs>
+        <rect
+          width="404"
+          height="404"
+          fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+        />
+      </svg>
+      <svg
+        class="absolute right-full bottom-0 transform -translate-x-1/2"
+        width="404"
+        height="404"
+        fill="none"
+        viewBox="0 0 404 404"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="85737c0e-0916-41d7-917f-596dc7edfa27"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect
+              x="0"
+              y="0"
+              width="4"
+              height="4"
+              class="text-gray-200"
+              fill="currentColor"
+            />
+          </pattern>
+        </defs>
+        <rect
+          width="404"
+          height="404"
+          fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+        />
+      </svg>
+      <div class="text-center">
+        <h2
+          class="
+            text-3xl
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          NuxtJs Video Trimmer
+        </h2>
+        <p class="mt-4 text-lg leading-6 text-gray-500">
+          This service enables us to upload videos for trimming. Upload video in
+          the form below to proceed.
+        </p>
+      </div>
+      <upload v-if="!uploadVideo" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import Upload from "../components/Upload.vue";
+export default {
+  components: {
+    Upload,
+  },
+  data() {
+    return {
+      uploadVideo: {
+        asset_id: "c082672c81c99157fbfaa87ed0345fae",
+        public_id: "nuxtjs-video-trimmer/uocakxjs3ddthxiegoku",
+        version: 1636256516,
+        version_id: "35217d98cebcbbd1523211b529828ddf",
+        signature: "10b53246bc211008b08252d8b5e42525c9883635",
+        width: 3840,
+        height: 2160,
+        format: "mp4",
+        resource_type: "video",
+        created_at: "2021-11-07T03:41:56Z",
+        tags: [],
+        pages: 0,
+        bytes: 22687114,
+        type: "upload",
+        etag: "e34d8ff931cb4864b23d797a78c77d59",
+        placeholder: false,
+        url: "http://res.cloudinary.com/hackit-africa/video/upload/v1636256516/nuxtjs-video-trimmer/uocakxjs3ddthxiegoku.mp4",
+        secure_url:
+          "https://res.cloudinary.com/hackit-africa/video/upload/v1636256516/nuxtjs-video-trimmer/uocakxjs3ddthxiegoku.mp4",
+        access_mode: "public",
+        audio: {
+          codec: "aac",
+          bit_rate: "253375",
+          frequency: 48000,
+          channels: 2,
+          channel_layout: "stereo",
+        },
+        video: {
+          pix_format: "yuv420p",
+          codec: "h264",
+          level: 52,
+          profile: "High",
+          bit_rate: "22006365",
+          time_base: "1/60",
+        },
+        is_audio: false,
+        frame_rate: 60,
+        bit_rate: 22215044,
+        duration: 8.17,
+        rotation: 0,
+        nb_frames: 489,
+      },
+    };
+  },
+};
 </script>
